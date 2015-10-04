04f8313ad94ff65c60db84637631de0ad4f252b0
Paul Wellner Bou <paul@wellnerbou.de>
2015-10-04 15:03:06 +0200

    Add email-ext plugin only when running with hpi:run

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -rf work/plugins
 MAVEN_OPTS="$MAVEN_OPTS -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000"
-mvn -Dmaven.test.skip=true -DskipTests=true -Djetty.port=9191 clean org.jenkins-ci.tools:maven-hpi-plugin:run -Pjenkins
+mvn -Dmaven.test.skip=true -DskipTests=true -Djetty.port=9191 clean compile org.jenkins-ci.tools:maven-hpi-plugin:run -Pjenkins,with-email-ext
