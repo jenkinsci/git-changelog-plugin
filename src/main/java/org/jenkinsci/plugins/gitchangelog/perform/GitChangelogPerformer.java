f849ae0afae28319ea5667d8f6b40e669e6fb7e6
Tomas Bjerre <tomas.bjerre85@gmail.com>
2016-03-10 18:59:41 +0100

    Trying to support slaves setup

--- src/main/java/org/jenkinsci/plugins/gitchangelog/perform/GitChangelogPerformer.java
+++ src/main/java/org/jenkinsci/plugins/gitchangelog/perform/GitChangelogPerformer.java
@@ -2,6 +2,7 @@ package org.jenkinsci.plugins.gitchangelog.perform;
 
 import static com.google.common.base.Charsets.UTF_8;
 import static com.google.common.base.Strings.isNullOrEmpty;
+import static com.google.common.base.Throwables.propagate;
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.io.Files.write;
 import static com.google.common.io.Resources.getResource;
@@ -14,129 +15,161 @@ import static org.jenkinsci.plugins.gitchangelog.config.GitChangelogConfigHelper
 import static org.jenkinsci.plugins.gitchangelog.config.GitChangelogConfigHelper.FROMTYPE.ref;
 import static se.bjurr.gitchangelog.api.GitChangelogApi.gitChangelogApiBuilder;
 import hudson.EnvVars;
+import hudson.FilePath;
+import hudson.FilePath.FileCallable;
 import hudson.model.BuildListener;
 import hudson.model.AbstractBuild;
+import hudson.remoting.VirtualChannel;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import org.jenkinsci.plugins.gitchangelog.config.CustomIssue;
 import org.jenkinsci.plugins.gitchangelog.config.GitChangelogConfig;
 import org.jenkinsci.plugins.gitchangelog.config.GitChangelogConfigHelper.FROMTYPE;
+import org.jenkinsci.remoting.RoleChecker;
 
 import se.bjurr.gitchangelog.api.GitChangelogApi;
 
 public class GitChangelogPerformer {
 
- public static void performerPerform(GitChangelogConfig config, AbstractBuild<?, ?> build, BuildListener listener) {
+ public static void performerPerform(final GitChangelogConfig configUnexpanded, final AbstractBuild<?, ?> build,
+   final BuildListener listener) {
   try {
    EnvVars env = build.getEnvironment(listener);
-   config = expand(config, env);
+   final GitChangelogConfig config = expand(configUnexpanded, env);
    listener.getLogger().println("---");
    listener.getLogger().println("--- Git Changelog ---");
    listener.getLogger().println("---");
 
    File workspace = new File(build.getExecutor().getCurrentWorkspace().toURI());
-   String workspacePath = workspace.getAbsolutePath();
+   FilePath fp = new FilePath(workspace);
+   fp.act(new FileCallable<Void>() {
 
-   GitChangelogApi gitChangelogApiBuilder = gitChangelogApiBuilder()//
-     .withFromRepo(workspacePath);
+    private static final long serialVersionUID = -9134176734041293984L;
 
-   if (config.isUseConfigFile() && !isNullOrEmpty(config.getConfigFile())) {
-    gitChangelogApiBuilder.withSettings(getResource(workspacePath + "/" + config.getConfigFile()).toURI().toURL());
-   }
+    @Override
+    public void checkRoles(RoleChecker arg0) throws SecurityException {
 
-   gitChangelogApiBuilder //
-     .withDateFormat(config.getDateFormat()) //
-     .withIgnoreCommitsWithMesssage(config.getIgnoreCommitsIfMessageMatches()) //
-     .withNoIssueName(config.getNoIssueName()) //
-     .withTimeZone(config.getTimeZone()) //
-     .withUntaggedName(config.getUntaggedName());
-
-   if (config.isUseJira()) {
-    gitChangelogApiBuilder //
-      .withJiraServer(config.getJiraServer()) //
-      .withJiraIssuePattern(config.getJiraIssuePattern()) //
-      .withJiraUsername(config.getJiraUsername()) //
-      .withJiraPassword(config.getJiraPassword());
-   }
-
-   if (config.isUseGitHub()) {
-    gitChangelogApiBuilder //
-      .withGitHubApi(config.getGitHubApi()) //
-      .withGitHubIssuePattern(config.getGitHubIssuePattern());
-   }
-
-   if (config.isUseReadableTagName() && !isNullOrEmpty(config.getReadableTagName())) {
-    gitChangelogApiBuilder.withReadableTagName(config.getReadableTagName());
-   }
-
-   if (isNullOrEmpty(config.getFromType()) || FROMTYPE.valueOf(config.getFromType()) == firstCommit) {
-    gitChangelogApiBuilder.withFromCommit(firstCommit.getReference());
-   } else if (FROMTYPE.valueOf(config.getFromType()) == ref && !isNullOrEmpty(config.getFromReference())) {
-    gitChangelogApiBuilder.withFromRef(config.getFromReference());
-   } else if (FROMTYPE.valueOf(config.getFromType()) == commit && !isNullOrEmpty(config.getFromReference())) {
-    gitChangelogApiBuilder.withFromCommit(config.getFromReference());
-   } else {
-    gitChangelogApiBuilder.withFromRef(firstCommit.getReference());
-   }
-
-   if (isNullOrEmpty(config.getToType()) || FROMTYPE.valueOf(config.getToType()) == master) {
-    gitChangelogApiBuilder.withToRef(master.getReference());
-   } else if (FROMTYPE.valueOf(config.getToType()) == ref && !isNullOrEmpty(config.getToReference())) {
-    gitChangelogApiBuilder.withToRef(config.getToReference());
-   } else if (FROMTYPE.valueOf(config.getToType()) == commit && !isNullOrEmpty(config.getToReference())) {
-    gitChangelogApiBuilder.withToCommit(config.getToReference());
-   } else {
-    gitChangelogApiBuilder.withToRef(firstCommit.getReference());
-   }
-
-   for (CustomIssue ci : config.getCustomIssues()) {
-    if (!isNullOrEmpty(ci.getName()) && !isNullOrEmpty(ci.getPattern())) {
-     gitChangelogApiBuilder.withCustomIssue(ci.getName(), ci.getPattern(), ci.getLink());
     }
-   }
 
-   if (config.showSummary()) {
-    if (config.isShowSummaryUseTemplateFile() && !isNullOrEmpty(config.getShowSummaryTemplateFile())) {
-     gitChangelogApiBuilder.withTemplatePath(workspacePath + "/" + config.getShowSummaryTemplateFile());
+    @Override
+    public Void invoke(File workspace, VirtualChannel channel) throws IOException, InterruptedException {
+     doPerform(build, listener, config, workspace);
+     return null;
     }
-    if (config.isShowSummaryUseTemplateContent() && !isNullOrEmpty(config.getShowSummaryTemplateContent())) {
-     gitChangelogApiBuilder.withTemplateContent(config.getShowSummaryTemplateContent());
-    }
-    build.addAction(new GitChangelogSummaryDecorator(gitChangelogApiBuilder.render()));
-   }
 
-   if (config.isUseMediaWiki()) {
-    if (config.isMediaWikiUseTemplateFile() && !isNullOrEmpty(config.getMediaWikiTemplateFile())) {
-     gitChangelogApiBuilder.withTemplatePath(workspacePath + "/" + config.getMediaWikiTemplateFile());
-    }
-    if (config.isMediaWikiUseTemplateContent() && !isNullOrEmpty(config.getMediaWikiTemplateContent())) {
-     gitChangelogApiBuilder.withTemplateContent(config.getMediaWikiTemplateContent());
-    }
-    String mediaWikiFullUrl = config.getMediaWikiUrl() + "/index.php/" + config.getMediaWikiTitle();
-    doLog(listener, INFO, "Posting changelog to " + mediaWikiFullUrl);
-    gitChangelogApiBuilder.toMediaWiki(//
-      config.getMediaWikiUsername(), //
-      config.getMediaWikiPassword(),//
-      config.getMediaWikiUrl(), //
-      config.getMediaWikiTitle());
+    private void doPerform(final AbstractBuild<?, ?> build, final BuildListener listener,
+      final GitChangelogConfig config, File workspace) throws MalformedURLException, IOException {
+     String workspacePath = workspace.getAbsolutePath();
 
-    build.addAction(new GitChangelogLeftsideBuildDecorator(config.getMediaWikiTitle(), mediaWikiFullUrl));
-   }
+     GitChangelogApi gitChangelogApiBuilder = gitChangelogApiBuilder()//
+       .withFromRepo(workspacePath);
 
-   if (config.isUseFile()) {
-    if (config.isCreateFileUseTemplateFile() && !isNullOrEmpty(config.getCreateFileTemplateFile())) {
-     gitChangelogApiBuilder.withTemplatePath(workspacePath + "/" + config.getCreateFileTemplateFile());
+     if (config.isUseConfigFile() && !isNullOrEmpty(config.getConfigFile())) {
+      try {
+       gitChangelogApiBuilder.withSettings(getResource(workspacePath + "/" + config.getConfigFile()).toURI().toURL());
+      } catch (URISyntaxException e) {
+       propagate(e);
+      }
+     }
+
+     gitChangelogApiBuilder //
+       .withDateFormat(config.getDateFormat()) //
+       .withIgnoreCommitsWithMesssage(config.getIgnoreCommitsIfMessageMatches()) //
+       .withNoIssueName(config.getNoIssueName()) //
+       .withTimeZone(config.getTimeZone()) //
+       .withUntaggedName(config.getUntaggedName());
+
+     if (config.isUseJira()) {
+      gitChangelogApiBuilder //
+        .withJiraServer(config.getJiraServer()) //
+        .withJiraIssuePattern(config.getJiraIssuePattern()) //
+        .withJiraUsername(config.getJiraUsername()) //
+        .withJiraPassword(config.getJiraPassword());
+     }
+
+     if (config.isUseGitHub()) {
+      gitChangelogApiBuilder //
+        .withGitHubApi(config.getGitHubApi()) //
+        .withGitHubIssuePattern(config.getGitHubIssuePattern());
+     }
+
+     if (config.isUseReadableTagName() && !isNullOrEmpty(config.getReadableTagName())) {
+      gitChangelogApiBuilder.withReadableTagName(config.getReadableTagName());
+     }
+
+     if (isNullOrEmpty(config.getFromType()) || FROMTYPE.valueOf(config.getFromType()) == firstCommit) {
+      gitChangelogApiBuilder.withFromCommit(firstCommit.getReference());
+     } else if (FROMTYPE.valueOf(config.getFromType()) == ref && !isNullOrEmpty(config.getFromReference())) {
+      gitChangelogApiBuilder.withFromRef(config.getFromReference());
+     } else if (FROMTYPE.valueOf(config.getFromType()) == commit && !isNullOrEmpty(config.getFromReference())) {
+      gitChangelogApiBuilder.withFromCommit(config.getFromReference());
+     } else {
+      gitChangelogApiBuilder.withFromRef(firstCommit.getReference());
+     }
+
+     if (isNullOrEmpty(config.getToType()) || FROMTYPE.valueOf(config.getToType()) == master) {
+      gitChangelogApiBuilder.withToRef(master.getReference());
+     } else if (FROMTYPE.valueOf(config.getToType()) == ref && !isNullOrEmpty(config.getToReference())) {
+      gitChangelogApiBuilder.withToRef(config.getToReference());
+     } else if (FROMTYPE.valueOf(config.getToType()) == commit && !isNullOrEmpty(config.getToReference())) {
+      gitChangelogApiBuilder.withToCommit(config.getToReference());
+     } else {
+      gitChangelogApiBuilder.withToRef(firstCommit.getReference());
+     }
+
+     for (CustomIssue ci : config.getCustomIssues()) {
+      if (!isNullOrEmpty(ci.getName()) && !isNullOrEmpty(ci.getPattern())) {
+       gitChangelogApiBuilder.withCustomIssue(ci.getName(), ci.getPattern(), ci.getLink());
+      }
+     }
+
+     if (config.showSummary()) {
+      if (config.isShowSummaryUseTemplateFile() && !isNullOrEmpty(config.getShowSummaryTemplateFile())) {
+       gitChangelogApiBuilder.withTemplatePath(workspacePath + "/" + config.getShowSummaryTemplateFile());
+      }
+      if (config.isShowSummaryUseTemplateContent() && !isNullOrEmpty(config.getShowSummaryTemplateContent())) {
+       gitChangelogApiBuilder.withTemplateContent(config.getShowSummaryTemplateContent());
+      }
+      build.addAction(new GitChangelogSummaryDecorator(gitChangelogApiBuilder.render()));
+     }
+
+     if (config.isUseMediaWiki()) {
+      if (config.isMediaWikiUseTemplateFile() && !isNullOrEmpty(config.getMediaWikiTemplateFile())) {
+       gitChangelogApiBuilder.withTemplatePath(workspacePath + "/" + config.getMediaWikiTemplateFile());
+      }
+      if (config.isMediaWikiUseTemplateContent() && !isNullOrEmpty(config.getMediaWikiTemplateContent())) {
+       gitChangelogApiBuilder.withTemplateContent(config.getMediaWikiTemplateContent());
+      }
+      String mediaWikiFullUrl = config.getMediaWikiUrl() + "/index.php/" + config.getMediaWikiTitle();
+      doLog(listener, INFO, "Posting changelog to " + mediaWikiFullUrl);
+      gitChangelogApiBuilder.toMediaWiki(//
+        config.getMediaWikiUsername(), //
+        config.getMediaWikiPassword(),//
+        config.getMediaWikiUrl(), //
+        config.getMediaWikiTitle());
+
+      build.addAction(new GitChangelogLeftsideBuildDecorator(config.getMediaWikiTitle(), mediaWikiFullUrl));
+     }
+
+     if (config.isUseFile()) {
+      if (config.isCreateFileUseTemplateFile() && !isNullOrEmpty(config.getCreateFileTemplateFile())) {
+       gitChangelogApiBuilder.withTemplatePath(workspacePath + "/" + config.getCreateFileTemplateFile());
+      }
+      if (config.isCreateFileUseTemplateContent() && !isNullOrEmpty(config.getCreateFileTemplateContent())) {
+       gitChangelogApiBuilder.withTemplateContent(config.getCreateFileTemplateContent());
+      }
+      doLog(listener, INFO, "Creating changelog " + config.toFile());
+      File toFile = new File(workspacePath + "/" + config.toFile());
+      new File(toFile.getParent()).mkdirs();
+      write(gitChangelogApiBuilder.render(), toFile, UTF_8);
+     }
     }
-    if (config.isCreateFileUseTemplateContent() && !isNullOrEmpty(config.getCreateFileTemplateContent())) {
-     gitChangelogApiBuilder.withTemplateContent(config.getCreateFileTemplateContent());
-    }
-    doLog(listener, INFO, "Creating changelog " + config.toFile());
-    File toFile = new File(workspacePath + "/" + config.toFile());
-    new File(toFile.getParent()).mkdirs();
-    write(gitChangelogApiBuilder.render(), toFile, UTF_8);
-   }
+   });
 
   } catch (Exception e) {
    doLog(listener, SEVERE, e.getMessage(), e);
