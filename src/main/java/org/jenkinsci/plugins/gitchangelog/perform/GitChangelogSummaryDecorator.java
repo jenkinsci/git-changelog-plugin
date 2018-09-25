7dd2e78895f71029f0c0720f44bd70e5332b5a18
Tomas Bjerre <tomas.bjerre85@gmail.com>
2018-09-25 17:24:40 +0200

    Re-enabling HTML on summary page #29

--- src/main/java/org/jenkinsci/plugins/gitchangelog/perform/GitChangelogSummaryDecorator.java
+++ src/main/java/org/jenkinsci/plugins/gitchangelog/perform/GitChangelogSummaryDecorator.java
@@ -1,11 +1,19 @@
 package org.jenkinsci.plugins.gitchangelog.perform;
 
+import hudson.markup.MarkupFormatter;
+import hudson.markup.RawHtmlMarkupFormatter;
 import hudson.model.Action;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.kohsuke.stapler.export.Exported;
 import org.kohsuke.stapler.export.ExportedBean;
 
 @ExportedBean(defaultVisibility = 2)
 public class GitChangelogSummaryDecorator implements Action {
+  private static final MarkupFormatter sanitizer = new RawHtmlMarkupFormatter(true);
+  private static Logger LOG = Logger.getLogger(GitChangelogSummaryDecorator.class.getSimpleName());
+
   private final String text;
 
   public GitChangelogSummaryDecorator(String text) {
@@ -24,7 +32,12 @@ public class GitChangelogSummaryDecorator implements Action {
 
   @Exported
   public String getText() {
-    return this.text;
+    try {
+      return sanitizer.translate(this.text);
+    } catch (IOException e) {
+      LOG.log(Level.SEVERE, e.getMessage(), e);
+      return "";
+    }
   }
 
   @Override
