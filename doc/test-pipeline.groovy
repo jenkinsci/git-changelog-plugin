e5f3e23faad20ae6ca001c55e9b61ea2830abd4f
Tomas Bjerre <tomas.bjerre85@gmail.com>
2024-09-18 16:47:38 +0200

    fix: using JGit 7 (refs #88)

---
+++ doc/test-pipeline.groovy
@@ -0,0 +1,46 @@
+node {
+ deleteDir()
+ sh """
+ git clone git@github.com:jenkinsci/git-changelog-plugin.git .
+ """
+
+ def changelogString = gitChangelog returnType: 'STRING',
+  from: [type: 'REF', value: 'git-changelog-1.50'],
+  to: [type: 'REF', value: 'master'],
+  template: """
+# Changelog
+
+Changelog for {{ownerName}} {{repoName}}.
+
+{{#tags}}
+## {{name}}
+ {{#issues}}
+  {{#hasIssue}}
+   {{#hasLink}}
+### {{name}} [{{issue}}]({{link}}) {{title}} {{#hasIssueType}} *{{issueType}}* {{/hasIssueType}} {{#hasLabels}} {{#labels}} *{{.}}* {{/labels}} {{/hasLabels}}
+   {{/hasLink}}
+   {{^hasLink}}
+### {{name}} {{issue}} {{title}} {{#hasIssueType}} *{{issueType}}* {{/hasIssueType}} {{#hasLabels}} {{#labels}} *{{.}}* {{/labels}} {{/hasLabels}}
+   {{/hasLink}}
+  {{/hasIssue}}
+  {{^hasIssue}}
+### {{name}}
+  {{/hasIssue}}
+
+  {{#commits}}
+**{{{messageTitle}}}**
+
+{{#messageBodyItems}}
+ * {{.}}
+{{/messageBodyItems}}
+
+[{{hash}}](https://github.com/{{ownerName}}/{{repoName}}/commit/{{hash}}) {{authorName}} *{{commitTime}}*
+
+  {{/commits}}
+
+ {{/issues}}
+{{/tags}}
+  """
+
+ currentBuild.description = changelogString
+}
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ./mvnw versions:update-properties
-./mvnw hpi:run -Djava.util.logging.config.file=logging.properties -Djenkins.version=2.401.2 -Denforcer.skip=true
+./mvnw hpi:run -Djava.util.logging.config.file=logging.properties -Djenkins.version=2.462.1 -Denforcer.skip=true
